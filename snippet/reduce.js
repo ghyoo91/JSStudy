67afc59d7b537ff27b29a24c65f8047d7bce5d53
ghyoo <ghyoo91@naver.com>
2022-09-25 21:07:50 +0900

    JS DOC practice

--- snippet/reduce.js
+++ snippet/reduce.js
@@ -1,8 +1,19 @@
+/**
+ * this is the detail of the customReduce.
+ * todo, deprecated 도 있음.
+ * @param {string} a 이건 aaaa
+ * @param {string} b 이건 bbbb
+ * @returns {string} 이건 A+B
+ */
+function customReduce(a, b) {
+    return a + b;
+}
+
 const a = [{id: 1, val: "eee"}];
 const b = [];
 const c = [{id: 1, val: "ddd"}, {id: 2, val: "kkk"}];
 const d = [{id: 1, val: "ddd"}, {id: 2, val: "kkk"}, {id: 3, val: "zzz"}, {id: 4, val: "qie"}];
-
+const e = customReduce(a, b);
 const func = (val) => val.reduce((pre, cur) => (pre.id > cur.id ? pre : cur));
 
 console.log(func(a).id);
