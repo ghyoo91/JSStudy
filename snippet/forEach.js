80fc11709cbaa724ecb4d3ddc44d880fddc88f16
ghyoo91_mac <ghyoo91@naver.com>
2023-02-25 15:40:42 +0900

    fakeshift, forEach

--- snippet/forEach.js
+++ snippet/forEach.js
@@ -1,23 +1,28 @@
 async function forEachTest() {
     const a = [];
-    const b = [];
-    const d = [];
 
     // 2097150 is the 2^21 - 2
-    for(let i=0;i<2097150;i++) {
+    for(let i=0;i<20;i++) {
         a.push(i);
-        b.push(i);
     }
 
     const c = a.map((val, idx) => {
-        if (val !== b[idx]) {
-            d.push(val);
-        }
+        return new Promise((resolve, reject) => {
+            if (idx === 10) {
+                reject();
+
+                return 3;
+            } else {
+                resolve();
+
+                return 1;
+            }
+        });
     });
 
-    await Promise.all(c);
+    const b = await Promise.all(c).catch(err => console.log(`ddd : ${err}`));
 
-    console.log(d);
+    console.log(b);
 }
 
 forEachTest();
